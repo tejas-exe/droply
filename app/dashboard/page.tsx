d031fd14e006b5cc0c5cb30b1806f102ef9f414c
tejas-exe <68724559+tejas-exe@users.noreply.github.com>
2025-05-28 11:03:33 +0530

    added
    added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,24 @@
-"use client";
 import React from "react";
-import { Card, CardBody } from "@heroui/react";
 
-const Dashboard = () => {
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Dashboard = async () => {
+  const { userId } = await auth();
+  const user = await currentUser();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#0F0E17] text-[#FCEAFF]">
-      <Card className="bg-[#1F1B2E] border border-[#A786DF] rounded-2xl shadow-[0_0_30px_#FF6EC7] p-10 max-w-xl mx-auto text-center">
-        <CardBody>
+      <div className="bg-[#1F1B2E] border border-[#A786DF] rounded-2xl shadow-[0_0_30px_#FF6EC7] p-10 max-w-xl mx-auto text-center">
+        <div>
           <h1 className="text-5xl md:text-6xl font-[cursive] text-[#FF6EC7] animate-pulse drop-shadow-[0_0_5px_#FF6EC7]">
             Upload And Share
           </h1>
-        </CardBody>
-      </Card>
+        </div>
+      </div>
     </div>
   );
 };
